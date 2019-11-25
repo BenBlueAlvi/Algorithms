dbbc5992973b19f1d69929cc67d50f1a9161b5c0
ben <benbluealvi@gmail.com>
2019-11-25 10:24:52 -0800

    Fixed a bug where the path length had to be n-1

--- hw9/dijkstra.py
+++ hw9/dijkstra.py
@@ -19,13 +19,21 @@ def shortest(n, edges):
 	adjlist = defaultdict(list)
 	for u, v, c in edges:
 		adjlist[u].append((c, v))
+		adjlist[v].append((c, u))
 
 		
 	back = defaultdict(int)
+	visited = set()
 	
 	while (h != []):
 		u = heappop(h)[1]
+		
+	
+		
+		if u in visited: continue
+		visited.add(u)
 		if u == n-1: break
+		
 		for c, v in adjlist[u]:
 			values[v] = min(values[v], values[u] + c)
 			
@@ -45,10 +53,14 @@ def shortest(n, edges):
 	if n-1 not in back: return None
 	#backtrace
 	output = [n-1]
-	for i in range(n-1):
+	i = 0
+	while (back[output[i]] != 0):
 		output.append(back[output[i]])
+		i += 1
+	output.append(0)
 		
 	output.reverse()
 	return (len(output), output)
-
-	
+print(shortest(5, [(0,1,1), (0,2,5), (1,2,1), (2,3,2), (1,3,6)])) #None
+print(shortest(8, [(0,4,2),(0,1,7),(0,7,12),(1,2,1),(1,3,1),(1,7,5),(2,3,3),(2,4,1),(2,5,1),(2,7,10),(3,6,2),(3,4,5),(3,7,1)]))
+#(6, [0, 4, 2, 1, 3, 7])
