aed56bcc6e2ff6484cc7e4e25fbfd2d0cfd8d9d9
ben <benbluealvi@gmail.com>
2019-11-25 17:48:12 -0800

    Added heapdict for speed

--- hw9/dijkstra.py
+++ hw9/dijkstra.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from heapq import heapify, heappop, heappush
+from heapdict import heapdict
 
 #use heapdict
 #complexity: O(v) pops O(vlogv) time for pops: O((V+E)logV)
@@ -11,7 +11,8 @@ def shortest(n, edges):
 	values[0] = 0
 	
 	
-	h = [(0,0)]
+	h = heapdict()
+	h[0] = 0
 	
 
 	
@@ -24,11 +25,10 @@ def shortest(n, edges):
 	back = defaultdict(int)
 	visited = set()
 	
-	while (h != []):
-		u = heappop(h)[1]
+	while (h):
+		u = h.popitem()[0]
 		
 	
-		
 		if u in visited: continue
 		visited.add(u)
 		if u == n-1: break
@@ -37,34 +37,22 @@ def shortest(n, edges):
 			if v in visited: continue
 			values[v] = min(values[v], values[u] + c)
 			
-			if values[v] == values[u] + c: back[v] = (u, c)
+			if values[v] == values[u] + c: back[v] = u
 			#print("v", v)
-			#decrease key
-			if v not in h:
-				heappush(h, (values[v], v))
+			
+			h[v] = values[v]
 				
-			else:
-				for k in range(len(h)):
-					if h[k][1] == v:
-						h[k][0] = values[v]
-						break
-				heapify(h)
 		#print(h)
 	if n-1 not in back: return None
 	#backtrace
 	output = [n-1]
 	
 	i = 0
-	while (back[output[i]][0] != 0):
-		output.append(back[output[i]][0])
+	while (back[output[i]] != 0):
+		output.append(back[output[i]])
 		
 		i += 1
 	output.append(0)
 	
 	output.reverse()
 	return (values[n-1], output)
-print(shortest(5, [(0,1,1), (0,2,5), (1,2,1), (2,3,2), (1,3,6)])) #None
-print(shortest(8, [(0,4,2),(0,1,7),(0,7,12),(1,2,1),(1,3,1),(1,7,5),(2,3,3),(2,4,1),(2,5,1),(2,7,10),(3,6,2),(3,4,5),(3,7,1)]))
-#(6, [0, 4, 2, 1, 3, 7])
-print(shortest(1000, [(0, 89, 10), (0, 221, 5), (0, 301, 20), (0, 331, 5), (0, 404, 16), (0, 728, 21), (0, 999, 27), (89, 451, 10), (89, 605, 5), (89, 728, 11), (89, 999, 16), (221, 236, 10), (221, 268, 9), (221, 382, 5), (221, 331, 5), (301, 331, 7), (301, 728, 8), (301, 999, 15), (331, 404, 7), (331, 473, 8), (331, 496, 10), (332, 534, 10), (331, 999, 30), (728, 996, 9), (996, 999, 5), (728, 999, 5)])) 
-# should return something like (25, [0, 331, 301, 728, 999])
